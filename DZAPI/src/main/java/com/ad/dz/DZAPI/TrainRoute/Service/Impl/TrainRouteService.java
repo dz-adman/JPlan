baf0878b766fdad30d6446c46d6a702b6aa1f987
Arun Kumar <36183057+dz-adman@users.noreply.github.com>
2019-10-27 22:07:18 +0530

    Added code to BY-PASS SSL for HTTPS Connection

--- DZAPI/src/main/java/com/ad/dz/DZAPI/TrainRoute/Service/Impl/TrainRouteService.java
+++ DZAPI/src/main/java/com/ad/dz/DZAPI/TrainRoute/Service/Impl/TrainRouteService.java
@@ -2,7 +2,16 @@ package com.ad.dz.DZAPI.TrainRoute.Service.Impl;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.URL;
+import java.net.URLConnection;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+
+import javax.net.ssl.X509TrustManager;
 
 import org.json.simple.JSONObject;
 import org.springframework.cache.annotation.Cacheable;
@@ -20,18 +29,38 @@ public class TrainRouteService implements ITrainRouteService{
 	{
 		try 
 		{
-			URL url = new URL("https://api.railwayapi.com/v2/route/train/"+ trainNum +"/apikey/"+ apiKey +"/");
+			// BY-PASS SSL for HTTPS Connection 	----Start
+			TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+			public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+				return null;
+			}
+			public void checkClientTrusted(X509Certificate[] certs, String authType) {
+			}
+			public void checkServerTrusted(X509Certificate[] certs, String authType) {
+			}
+			}};
+
+			SSLContext sc = SSLContext.getInstance("SSL");
+			sc.init(null, trustAllCerts, new java.security.SecureRandom());
+			HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+			// BY-PASS SSL for HTTPS Connection 	----End
+			
+			URL url = new URL("https://indianrailapi.com/api/v2/TrainSchedule/apikey/" + apiKey + "/TrainNumber/" + trainNum + "/");
 			System.out.println(url);
-			BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+
+			URLConnection con = url.openConnection();
+			Reader reader = new InputStreamReader(con.getInputStream());
+			BufferedReader br = new BufferedReader(reader);
+
 			String str = "";
 			StringBuffer sx = new StringBuffer();
-			while (null != (str = br.readLine())) {
+			while ((str = br.readLine()) != null) {
 				System.out.println(str);
 				sx.append(str);
 			}
 			String x = sx.toString();
 			return x;
-		} 
+		}
 		catch (Exception ex) 
 		{
 			ex.printStackTrace();
@@ -40,5 +69,4 @@ public class TrainRouteService implements ITrainRouteService{
 			return jObj.toString();
 		}
 	}
-
 }
