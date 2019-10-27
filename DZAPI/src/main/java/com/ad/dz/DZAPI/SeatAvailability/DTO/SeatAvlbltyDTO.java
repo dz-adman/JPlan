d6ac60164cf72edf1703025c6f8c7aa9abaaf36e
Arun Kumar <36183057+dz-adman@users.noreply.github.com>
2019-10-27 21:54:55 +0530

    Added @ApiModelProperty annotations for Swagger

--- DZAPI/src/main/java/com/ad/dz/DZAPI/SeatAvailability/DTO/SeatAvlbltyDTO.java
+++ DZAPI/src/main/java/com/ad/dz/DZAPI/SeatAvailability/DTO/SeatAvlbltyDTO.java
@@ -1,27 +1,34 @@
 package com.ad.dz.DZAPI.SeatAvailability.DTO;
 
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import io.swagger.annotations.ApiParam;
 
 @ApiModel(value = "SeatAvailability", description = "Seat Availability Object")
 public class SeatAvlbltyDTO {
 
 	@ApiParam(value = "Train Number", required = true)
+	@ApiModelProperty(value = "Train Number", required = true)
 	private int trainNum;
 	
 	@ApiParam(value = "Source Station Code", required = true)
+	@ApiModelProperty(value = "Source Station Code", required = true)
 	private String srcStn;
 	
 	@ApiParam(value = "Destination Station Code", required = true)
+	@ApiModelProperty(value = "Destination Station Code", required = true)
 	private String destStn;
 	
-	@ApiParam(value = "Journey Date", required = true)
+	@ApiParam(value = "Journey Date (DD-MM-YYYY)", required = true)
+	@ApiModelProperty(value = "Journey Date (DD-MM-YYYY)", required = true)
 	private String jDate;
 	
 	@ApiParam(value = "Travelling Class", required = true)
+	@ApiModelProperty(value = "Travelling Class", required = true)
 	private String trvlCls;
 	
 	@ApiParam(value = "Quota", required = true, allowableValues = "GN - General Quota, CK - Tatkal Quota, LD - Ladies Quota, DF - Defence Quota, PH - Parliament House Quota, FT - Foreign Tourist Quota, DP - Duty Pass Quota, SS - Female(above 45 Year)/Senior Citizen/Travelling alone, HP - Physically Handicapped Quota, RE - Railway Employee Staff on Duty for the train, YU - Yuva Quota, LB - Lower Birth Quota")
+	@ApiModelProperty(value = "Quota (GN)", required = true)
 	private String quota;
 	
 	public SeatAvlbltyDTO(int trainNum, String srcStn, String destStn, String jDate, String trvlCls, String quota) {
