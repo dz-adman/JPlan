a7aac0da3bee04cb1a6d2cf51a96c7eed9fee9cd
Arun Kumar <36183057+dz-adman@users.noreply.github.com>
2019-10-27 21:57:44 +0530

    Added code to BY-PASS SSL for HTTPS Connection

--- DZAPI/src/main/java/com/ad/dz/DZAPI/SeatAvailability/Service/Impl/SeatAvlbltyService.java
+++ DZAPI/src/main/java/com/ad/dz/DZAPI/SeatAvailability/Service/Impl/SeatAvlbltyService.java
@@ -2,7 +2,16 @@ package com.ad.dz.DZAPI.SeatAvailability.Service.Impl;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.URL;
+import java.net.URLConnection;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+
+import javax.net.ssl.X509TrustManager;
 
 import org.json.simple.JSONObject;
 import org.springframework.cache.annotation.Cacheable;
@@ -22,9 +31,29 @@ public class SeatAvlbltyService implements ISeatAvlbltyService{
 		seatAvlbltyDTO.setQuota(seatAvlbltyDTO.getQuota().substring(0,seatAvlbltyDTO.getQuota().indexOf(' ')));
 		try 
 		{
-			URL url = new URL("https://api.railwayapi.com/v2/check-seat/train/"+ seatAvlbltyDTO.getTrainNum() +"/source/"+ seatAvlbltyDTO.getSrcStn() +"/dest/"+ seatAvlbltyDTO.getDestStn() +"/date/"+ seatAvlbltyDTO.getjDate() +"/pref/"+ seatAvlbltyDTO.getTrvlCls() +"/quota/GN/apikey/"+ apiKey +"/");
+			// BY-PASS SSL for HTTPS Connection 	----Start
+			TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+			public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+				return null;
+			}
+			public void checkClientTrusted(X509Certificate[] certs, String authType) {
+			}
+			public void checkServerTrusted(X509Certificate[] certs, String authType) {
+			}
+			}};
+
+			SSLContext sc = SSLContext.getInstance("SSL");
+			sc.init(null, trustAllCerts, new java.security.SecureRandom());
+			HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+			// BY-PASS SSL for HTTPS Connection 	----End
+			
+			URL url = new URL("https://indianrailapi.com/api/v2/SeatAvailability/apikey/" + apiKey + "/TrainNumber/" + seatAvlbltyDTO.getTrainNum() + "/From/" + seatAvlbltyDTO.getSrcStn() + "/To/" + seatAvlbltyDTO.getDestStn() + "/Date/" + seatAvlbltyDTO.getjDate() + "/Quota/GN/Class/" + seatAvlbltyDTO.getTrvlCls() + "/");
 			System.out.println(url);
-			BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+			
+			URLConnection con = url.openConnection();
+	        	Reader reader = new InputStreamReader(con.getInputStream());
+	        	BufferedReader br = new BufferedReader(reader);
+	        
 			String str = "";
 			StringBuffer sx = new StringBuffer();
 			while (null != (str = br.readLine())) {
